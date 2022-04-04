ede11f1c6f8eb8f397d60945194d3e33380511d9
et9719 <ella-t@live.co.uk>
2022-04-04 21:15:47 +0000

    Added sizes to product model and size selector box to bag template

--- products/models.py
+++ products/models.py
@@ -17,10 +17,12 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    ''' Add description '''
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
